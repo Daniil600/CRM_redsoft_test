fbc84d7b7bfcdf50f52622e0dead26e91aa8acb2
Daniil600 <ter6062@ya.ru>
2023-07-30 15:01:43 +0300

    update crm.sql add default values for departments and positions

--- src/main/resources/scripts/crm.sql
+++ src/main/resources/scripts/crm.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS positions
 CREATE TABLE IF NOT EXISTS employees
 (
     employee_id   SERIAL PRIMARY KEY,
-    first_name    VARCHAR(255),
-    last_name     VARCHAR(255),
+    first_name    VARCHAR(255) NOT NULL,
+    last_name     VARCHAR(255) NOT NULL,
     department_id INTEGER,
     position_id   INTEGER REFERENCES positions (position_id)
 );
@@ -26,3 +26,14 @@ CREATE TABLE IF NOT EXISTS departments
 
 
 ALTER TABLE employees ADD FOREIGN KEY (department_id) REFERENCES departments(department_id);
+
+INSERT INTO departments (department_name, number, email)
+VALUES ('IT', 84821569843, 'it@gmail.com'),
+       ('Design', 84821223456, 'design@gmail.com'),
+       ('Accounting', 8482785321, 'accounting@gmail.com');
+
+INSERT INTO positions (position_name, salary)
+VALUES ('Director', 420000.00),
+       ('Developer', 150000),
+       ('Employee', 80000);
+
